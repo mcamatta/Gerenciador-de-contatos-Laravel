0237338e7a4bf59dd6708d96a322049f20080b10
mcamatta <camattamichel@gmail.com>
2023-07-29 12:09:32 -0300

    feat: display errors messages

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -1,13 +1,35 @@
 <x-app-layout title="Contact - Create">
-    
     <form action="{{route('contacts.store')}}" method="POST">
         @csrf
-        <label for="name">Name</label>
-        <input type="text" name="name">
-        <label for="contact">Contact</label>
-        <input type="text" name="contact">
-        <label for="name">E-mail</label>
-        <input type="email" name="email">
+        <div>
+            <div>
+                <label for="name">Name</label>
+                <input type="text" name="name" value="{{ old('name', $contact->name) }}">
+            </div>
+            @error('name')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+        <div>
+            <div>
+                <label for="contact">Contact</label>
+                <input type="text" name="contact" value="{{ old('contact', $contact->contact) }}"  min="9" max="9">
+            </div>
+            @error('text')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div>
+            <div>
+                <label for="email">E-mail</label>
+                <input type="email" name="email" value="{{ old('email', $contact->email) }}">
+            </div>
+            @error('email')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+
         <button type="submit">Send</button>
     </form>
 </x-app-layout>
--- resources/views/contacts/edit.blade.php
+++ resources/views/contacts/edit.blade.php
@@ -2,15 +2,35 @@
     <form action="{{route('contacts.update', $contact->id)}}" method="POST">
         @csrf
         @method('patch')
-        <label for="name">Name</label>
-        <input type="text" name="name" value="{{ old('name', $contact->name) }}">
-        
-        <label for="contact">Contact</label>
-        <input type="text" name="contact" value="{{ old('contact', $contact->contact) }}">
-        
-        <label for="email">E-mail</label>
-        <input type="email" name="email" value="{{ old('email', $contact->email) }}">
-        
+        <div>
+            <div>
+                <label for="name">Name</label>
+                <input type="text" name="name" value="{{ old('name', $contact->name) }}">
+            </div>
+            @error('name')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+        <div>
+            <div>
+                <label for="contact">Contact</label>
+                <input type="text" name="contact" value="{{ old('contact', $contact->contact) }}"  min="9" max="9">
+            </div>
+            @error('text')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div>
+            <div>
+                <label for="email">E-mail</label>
+                <input type="email" name="email" value="{{ old('email', $contact->email) }}">
+            </div>
+            @error('email')
+                <p class="error-message">{{ $message }}</p>
+            @enderror
+        </div>
+
         <button type="submit">Send</button>
     </form>
 </x-app-layout>
