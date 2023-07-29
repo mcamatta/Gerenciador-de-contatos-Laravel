38120e01c1c7f879faba9a0349d3163e8e18b675
mcamatta <camattamichel@gmail.com>
2023-07-29 09:27:05 -0300

    feat: Migration for contacts table

---
+++ database/migrations/2023_07_29_121705_create_contacts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128);
+            $table->string('contact', 9)->unique();
+            $table->string('email', 128)->unique();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
